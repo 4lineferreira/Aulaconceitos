df1700796d26cd27205ca6a0a20ac4372a4065de
4lineferreira <alineferreirasnsd@gmail.com>
2024-11-12 10:21:55 -0300

    Teste de commit na nova branch Dev

---
+++ teste/TesteBranchDev.java
@@ -0,0 +1,22 @@
+package teste;
+
+public class TesteBranchDev {
+
+	private String teste;
+
+	public TesteBranchDev(String teste) {
+		super();
+		this.teste = teste;
+	}
+
+	public String getTeste() {
+		return teste;
+	}
+
+	public void setTeste(String teste) {
+		this.teste = teste;
+	}
+	
+	
+	
+}
